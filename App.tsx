33f34b572558c84aeab1d5888bb903b22afc4fdb
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-11-01 00:13:33 -0300

    chore: add Roboto font

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Text, Center } from 'native-base';
 
+import { THEME } from "./src/styles/theme"
+
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <Center flex={1} bgColor="black" alignItems="center" justifyContent="center">
-        <Text color="white" fontSize={24}>FIFA World Cup!</Text>
+        <Text color="yellow.600" fontSize={24}>FIFA World Cup!</Text>
         <StatusBar style="auto" />
       </Center>
     </NativeBaseProvider>
