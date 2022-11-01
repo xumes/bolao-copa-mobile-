bf77bdefb1d634b85daf54860b95677a4f1534cd
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-11-01 00:29:26 -0300

    feat: customize status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto"
 
 import { Loading} from "./src/components/Loading"
@@ -11,6 +11,12 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
       {
         fontsLoaded ? <SignIn /> : <Loading />
       }
