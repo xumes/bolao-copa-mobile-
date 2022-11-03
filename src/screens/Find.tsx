0d0b2b3ef2f52208fb7ddec4fbe0ef17c630f8ac
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-11-02 23:20:21 -0300

    feat: add Find Bolao screen

---
+++ src/screens/Find.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Heading, Text, VStack } from "native-base";
+import { Header } from '../components/Header';
+
+import Logo from '../assets/logo.svg'
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+
+export function Find() {
+    return (
+        <VStack  flex={1} bgColor="black">
+            <Header title="Buscar por código" showBackButton/>
+
+            <VStack mt={8} mx={5} alignItems="center">
+                <Logo />
+
+                <Heading fontFamily="heading" color="white" fontSize="xl" my={8} textAlign="center">
+                    Encontrar o bolão através de{'\n'}
+                    seu código único!
+                </Heading>
+
+                <Input 
+                    mb={2}
+                    placeholder="Qual o código do bolão?"
+                />
+
+                <Button 
+                    title="Buscar bolão"
+                />
+            </VStack>
+        </VStack>
+    )
+}
