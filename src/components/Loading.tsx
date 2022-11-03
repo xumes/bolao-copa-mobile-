1a90d0f80985c7247c7a49ce412128bac82918a8
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-11-02 23:15:12 -0300

    feat: add Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,10 @@
-import React from "react"
-import {Center,Spinner} from "native-base"
+import React from 'react';
+import { Center, Spinner } from "native-base";
 
 export function Loading() {
-    return (
-        <Center flex={1} bg="gray.900">
-            <Spinner color="yellow.500" />
-        </Center>
-    )
+  return (
+    <Center flex={1} bg="gray.900">
+      <Spinner color="yellow.500" />
+    </Center >
+  );
 }
